63858962358e1beef87c75ca7226c3aa43667424
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2020-01-27 22:25:29 +0300

    Update list on scroll

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    <vk-card
-      @scroll="checkScroll"
-      v-for="(user, index) in users"
+    <vk-card 
+      v-for="(user, index) in users" 
+      :key="index" 
       class="uk-width-1-2 centered"
-      :key="index"
-      ref="index"
     >
       <div slot="header">
-        <vk-grid gutter="small" class="uk-flex-middle">
+        <vk-grid 
+          gutter="small" 
+          class="uk-flex-middle"
+        >
           <div class="uk-width-auto">
-            <img class="uk-border-circle" width="80" height="80" :src="user.picture.large" />
+            <img 
+              class="uk-border-circle" 
+              width="80" 
+              height="80" 
+              :src="user.picture.large"
+            >
           </div>
           <div class="uk-width-expand">
-            <vk-card-title class="uk-margin-remove-bottom">{{ fullName(user.name) }}</vk-card-title>
+            <vk-card-title class="uk-margin-remove-bottom">
+              {{ fullName(user.name) }}
+            </vk-card-title>
             <p class="uk-text-meta uk-margin-remove-top">
               <span>{{ user.email }}</span>
             </p>
@@ -26,31 +34,45 @@
 
 <script>
 export default {
-  name: "main-card",
+  name: "MainCard",
   data() {
     return {
+      uploading: false,
       users: []
     };
   },
   created() {
-    fetch("https://randomuser.me/api/?results=10&inc=name,picture,email")
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("API Error");
-      })
-      .then(body => {
-        this.$set(this, "users", body.results);
-        console.log(this.users);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    window.addEventListener('scroll', this.handleScroll);
+    this.loadNewUsers(10);
   },
   methods: {
     fullName(name) {
       return `${name.title} ${name.first} ${name.last}`;
+    },
+    handleScroll() {
+      const position = window.scrollY + window.innerHeight;
+      const bottom = document.body.scrollHeight;
+      const offset = 400;
+      if(position >= bottom - offset && !this.uploading) {
+        this.loadNewUsers(15);
+      }
+    },
+    loadNewUsers(amount) {
+      this.$set(this, 'uploading', true);
+      fetch(`https://randomuser.me/api/?results=${amount}&inc=name,picture,email`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("API Error");
+        })
+        .then(body => {
+          this.$set(this, "users", [...this.users, ...body.results]);
+          this.$set(this, 'uploading', false);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 };
@@ -59,5 +81,10 @@ export default {
 <style>
 .centered {
   margin: 1% auto;
+}
+
+.centered:last-child {
+  margin-top: 0;
+  margin-bottom: 0;
 }
 </style>
