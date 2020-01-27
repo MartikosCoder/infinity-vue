bc8d80bc6d3b9d9bec60693ec233e578f589abc5
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2020-01-27 21:41:28 +0300

    Render first 10 persons

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,59 @@
 <template>
-  <vk-card class="uk-width-1-2 centered">
-    <div slot="header">
-      <vk-grid gutter="small" class="uk-flex-middle">
-        <div class="uk-width-auto">
-          <img class="uk-border-circle" width="40" height="40" src="/images/guide/avatar.jpg" />
-        </div>
-        <div class="uk-width-expand">
-          <vk-card-title class="uk-margin-remove-bottom">Title</vk-card-title>
-          <p class="uk-text-meta uk-margin-remove-top">
-            <time datetime="2016-04-01T19:00">April 01, 2016</time>
-          </p>
-        </div>
-      </vk-grid>
-    </div>
-    <div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-    </div>
-    <div slot="footer">
-      <vk-button-link type="text" href="#">Read more</vk-button-link>
-    </div>
-  </vk-card>
+  <div>
+    <vk-card
+      @scroll="checkScroll"
+      v-for="(user, index) in users"
+      class="uk-width-1-2 centered"
+      :key="index"
+      ref="index"
+    >
+      <div slot="header">
+        <vk-grid gutter="small" class="uk-flex-middle">
+          <div class="uk-width-auto">
+            <img class="uk-border-circle" width="80" height="80" :src="user.picture.large" />
+          </div>
+          <div class="uk-width-expand">
+            <vk-card-title class="uk-margin-remove-bottom">{{ fullName(user.name) }}</vk-card-title>
+            <p class="uk-text-meta uk-margin-remove-top">
+              <span>{{ user.email }}</span>
+            </p>
+          </div>
+        </vk-grid>
+      </div>
+    </vk-card>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "main-card",
+  data() {
+    return {
+      users: []
+    };
+  },
+  created() {
+    fetch("https://randomuser.me/api/?results=10&inc=name,picture,email")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("API Error");
+      })
+      .then(body => {
+        this.$set(this, "users", body.results);
+        console.log(this.users);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  methods: {
+    fullName(name) {
+      return `${name.title} ${name.first} ${name.last}`;
+    }
+  }
+};
 </script>
 
 <style>
